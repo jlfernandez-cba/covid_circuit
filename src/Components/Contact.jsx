5d300a819f31d8c8c470a2687b8bd74d067c9f0d
Rubén García Serrano <onitaku@gmail.com>
2021-06-11 09:29:39 +0200

    map responsive update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,35 +4,39 @@ export default function Contact() {
         <div id="contact" className="page-section">
             <div className="container">
                 <div className="row">
-                <div className="col-md-12">
-                    <div className="section-heading">
-                    <h4>Contact</h4>
-                    <div className="line-dec" />
+                    <div className="col-md-12">
+                        <div className="section-heading">
+                            <h4>Contact</h4>
+                            <div className="line-dec" />
+                        </div>
                     </div>
-                </div>
                 </div>
                 <div className="row">
-                <div className="col-md-6">
-                    <div className="map">
-                    <iframe allowFullScreen frameBorder={0} height={300} width={400} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1585.604945582641!2d-5.9802101!3d37.3612102!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x70784b65432acca!2sCentro+de+Documentaci%C3%B3n+Cl%C3%ADnica+Avanzada!5e0!3m2!1sen!2ses!4v1503409126736" style={{border: 0}} />
+                    <div className="col-md-6">
+                        <div className="embed-responsive embed-responsive-16by9">
+                            <iframe
+                                title="centro de documentación clínica"
+                                className="embed-responsive-item"
+                                frameborder="0"
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1585.604945582641!2d-5.9802101!3d37.3612102!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x70784b65432acca!2sCentro+de+Documentaci%C3%B3n+Cl%C3%ADnica+Avanzada!5e0!3m2!1sen!2ses!4v1503409126736" ></iframe>
+                        </div>
+                        <br />
                     </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="row">
-                    <div className="contacto">
-                        <p><a href="http://www.clinbioinfosspa.es/">Clinical Bioinformatics Area</a>, <a href="https://www.sspa.juntadeandalucia.es/fundacionprogresoysalud/">Fundación Progreso y Salud</a>.</p>
-                        <p>Edificio C.D.C.A.. Hospital Universitario  Virgen del Rocío
+                    <div className="col-md-6">
+                        <div className="row">
+                            <div className="contacto">
+                                <p><a href="http://www.clinbioinfosspa.es/">Clinical Bioinformatics Area</a>, <a href="https://www.sspa.juntadeandalucia.es/fundacionprogresoysalud/">Fundación Progreso y Salud</a>.</p>
+                                <p>Edificio C.D.C.A.. Hospital Universitario  Virgen del Rocío
                         Avenida Manuel Siurot s/n,</p>
-                        <p>41013 Sevilla, España</p>
-                        <br />
-                        <br />
-                        <p>Email: <a style={{color: '#97B4CF'}} href="mailto:clinbioinfoarea@gmail.com">clinbioinfoarea@gmail.com</a></p>
+                                <p>41013 Sevilla, España</p>
+                                <br />
+                                <br />
+                                <p>Email: <a style={{ color: '#97B4CF' }} href="mailto:clinbioinfoarea@gmail.com">clinbioinfoarea@gmail.com</a></p>
+                            </div>
+                        </div>
                     </div>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
-
     )
 }
